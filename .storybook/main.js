43d11e7a7030800778f60d031920795a4d2e19a0
Mostafa Talaat <mostafatalaat770@gmail.com>
2022-09-13 10:06:34 +0200

    config: add scss modules support

--- .storybook/main.js
+++ .storybook/main.js
@@ -7,6 +7,28 @@ module.exports = {
 		"@storybook/addon-links",
 		"@storybook/addon-essentials",
 		"@storybook/addon-interactions",
+		{
+			name: `@storybook/preset-scss`,
+			options: {
+				rule: {
+					test: /(?<!\.module).s[ca]ss$/,
+				},
+			},
+		},
+		// module
+		{
+			name: `@storybook/preset-scss`,
+			options: {
+				rule: {
+					test: /\.module\.s[ca]ss$/,
+				},
+				cssLoaderOptions: {
+					modules: {
+						localIdentName: "[name]__[local]--[hash:base64:5]",
+					},
+				},
+			},
+		},
 	],
 	framework: "@storybook/react",
 	core: {
