e3fe7a4f9ddb6658b6e23b39e8217c29a5008696
Mostafa Talaat <mostafatalaat770@gmail.com>
2022-09-13 15:27:01 +0200

    add types

---
+++ src/@types/images.d.ts
@@ -0,0 +1,9 @@
+declare module "*.svg" {
+	import React = require("react");
+
+	export const ReactComponent: React.FC<
+		React.PropsWithChildren<React.SVGProps<SVGSVGElement>>
+	>;
+	const src: string;
+	export default src;
+}
